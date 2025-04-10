a2622cc1c7a120e14de22792dd569072f31b0538
jensuki <jeni.suk@proton.me>
2025-04-10 15:21:44 -0700

    add scripts for dark mode toggling and icon switching

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
-// if (
-//     localStorage.getItem('color-theme') === 'dark' ||
-//     (!('color-theme' in localStorage) &&
-//         window.matchMedia('(prefers-color-scheme: dark)').matches)
-// ) {
-//     document.documentElement.classList.add('dark');
-// } else {
-//     document.documentElement.classList.remove('dark');
-// }
+// grab our button and icons
+const toggleBtn = document.querySelector('#theme-toggle');
+const darkIcon = document.querySelector('#theme-toggle-dark-icon');
+const lightIcon = document.querySelector('#theme-toggle-light-icon');
+
+// determine initial theme
+const currentTheme = localStorage.getItem('color-theme'); // if exists
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const isDark = currentTheme === 'dark' || (!currentTheme && systemPrefersDark);
+
+//  set appropriate icon on page load based on current theme
+darkIcon.classList.toggle('hidden', isDark);
+lightIcon.classList.toggle('hidden', !isDark);
+
+// toggle event
+toggleBtn.addEventListener('click', () => {
+    const isDarkMode = document.documentElement.classList.toggle('dark');
+    //save to local storage
+    localStorage.setItem('color-theme', isDarkMode ? 'dark' : 'light');
+
+    // update icons
+    darkIcon.classList.toggle('hidden', isDarkMode);
+    lightIcon.classList.toggle('hidden', !isDarkMode);
+})
